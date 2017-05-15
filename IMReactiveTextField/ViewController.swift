27ff7aa5e4b4666f33a25c6a70e57dc5a28ef61b
Ivan <idion917@gmail.com>
2017-05-15 12:09:11 -0400

    Initial Commit

---
+++ IMReactiveTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMReactiveTextField
+//
+//  Created by Iván Alejandro Moya Quilodrán on 15-05-17.
+//  Copyright © 2017 Iván Alejandro Moya Quilodrán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
